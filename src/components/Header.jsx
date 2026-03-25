32ce85d20a31b44c599d3a0e76e4ae7949e47e20
marefpceo <marefpceo@gmail.com>
2026-03-24 20:41:25 -0700

    Created header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import logoSmall from '../assets/logo-150w.webp';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+function Header() {
+  return (
+    <header className='p-2 flex justify-between items-center'>
+      <img src={logoSmall} alt='Logo' width={150} />
+      <FontAwesomeIcon
+        icon={faBars}
+        fontSize={48}
+        className='text-nature-light'
+      />
+    </header>
+  );
+}
+
+export default Header;
