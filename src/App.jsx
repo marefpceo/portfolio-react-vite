cc0a3fd69ba93374844d15027479ca159558196c
marefpceo <32721274+marefpceo@users.noreply.github.com>
2026-03-25 17:00:16 -0700

    Added background images to layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <div className='md:max-w-full lg:max-w-4/5 mx-auto'>
-      <Header />
-      <Outlet />
+    <div
+      className='container min-w-full h-full bg-[url(./assets/portfolioBg-Desktop.webp)] bg-cover
+      bg-center 2xl:bg-[url(./assets/portfolioBg-4k.webp)]'
+    >
+      <div role='main' className='md:max-w-full lg:max-w-4/5 mx-auto '>
+        <Header />
+        <Outlet />
+      </div>
     </div>
   );
 }
