077c16595ceffe2d8f4e8ba3f67b543a2f52a156
marefpceo <marefpceo@gmail.com>
2026-03-26 17:07:02 -0700

    Configured MenuModal with functional logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,44 @@
+import { useState } from 'react';
 import logoSmall from '../assets/logo-150w.webp';
+import MenuModal from './MenuModal';
 import NavBar from './NavBar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function openMenuModal() {
+    setIsOpen(true);
+  }
+
+  function closeMenuModal() {
+    setIsOpen(false);
+  }
+
   return (
-    <header role='banner' className='p-2 flex justify-between items-center'>
-      <img src={logoSmall} alt='Logo' width={150} height={69} loading='lazy' />
-      {/* Hide mobile menu bar for screen widths >= 768 */}
-      <span className='flex md:hidden'>
-        <FontAwesomeIcon
-          icon={faBars}
-          fontSize={44}
-          className='text-nature-light'
+    <>
+      <MenuModal open={isOpen} closeMenuModal={closeMenuModal} />
+      <header role='banner' className='p-2 flex justify-between items-center'>
+        <img
+          src={logoSmall}
+          alt='Logo'
+          width={150}
+          height={69}
+          loading='lazy'
         />
-      </span>
-      <NavBar />
-    </header>
+        {/* Hide mobile menu bar for screen widths >= 768 */}
+        <span className='flex md:hidden' onClick={openMenuModal}>
+          <FontAwesomeIcon
+            icon={faBars}
+            fontSize={44}
+            className='text-nature-light'
+          />
+        </span>
+
+        <NavBar />
+      </header>
+    </>
   );
 }
 
--- src/components/MenuModal.jsx
+++ src/components/MenuModal.jsx
@@ -1,40 +1,52 @@
+import { Link, useLocation } from 'react-router';
 import useLockBodyScroll from '../hooks/useLockBodyScroll';
 import useEscapeKey from '../hooks/useEscapeKey';
-import { RiCloseLargeFill } from 'react-icons/ri';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 export default function MenuModal({ open, closeMenuModal }) {
+  const location = useLocation();
   const showMenu = open === true ? 'flex' : 'hidden';
 
   useLockBodyScroll(open);
-
   useEscapeKey(closeMenuModal);
 
   return (
     <div
-      className={`${showMenu} bg-nature-dark/85  absolute min-h-lvh w-full z-50`}
+      className={`${showMenu} bg-nature-altLight absolute min-h-lvh w-full z-50`}
     >
-      <nav className={` w-full p-2 bg-nature-alt`}>
+      <nav className={`w-full p-2 bg-nature-alt`}>
         <div>
-          <RiCloseLargeFill
-            className='absolute right-4 top-4 text-nature-accent'
-            size={24}
+          <FontAwesomeIcon
+            icon={faXmark}
+            className='absolute p-1 right-4 top-4 text-nature-accent bg-nature-altDark rounded-full'
+            fontSize={32}
             onClick={closeMenuModal}
           />
-          <ul className='mx-4 mt-8 p-4 flex flex-col items-center text-lg gap-8 '>
-            <li>
-              <a href='#about' onClick={closeMenuModal}>
+          <ul className='mx-4 mt-20 p-4 flex flex-col items-center text-xl gap-8 '>
+            <li className={`${location.pathname === '/' ? 'hidden' : ''}`}>
+              <Link to={'/'} onClick={closeMenuModal}>
+                Home
+              </Link>
+            </li>
+            <li className={`${location.pathname === '/about' ? 'hidden' : ''}`}>
+              <Link to={'/about'} onClick={closeMenuModal}>
                 About
-              </a>
+              </Link>
             </li>
-            <li>
-              <a href='#projects' onClick={closeMenuModal}>
+            <li
+              className={`${location.pathname === '/projects' ? 'hidden' : ''}`}
+            >
+              <Link to={'#projects'} onClick={closeMenuModal}>
                 Projects
-              </a>
+              </Link>
             </li>
-            <li>
-              <a href='#contact' onClick={closeMenuModal}>
+            <li
+              className={`${location.pathname === '/contact' ? 'hidden' : ''}`}
+            >
+              <Link to={'#contact'} onClick={closeMenuModal}>
                 Contacts
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
