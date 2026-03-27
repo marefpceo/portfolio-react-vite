344ef4b74210a113676ee75c1c313673a02946cb
marefpceo <marefpceo@gmail.com>
2026-03-26 17:56:57 -0700

    Updated NavBar logic

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,33 @@
-import { Link } from 'react-router';
+import { Link, useLocation } from 'react-router';
 
 function NavBar() {
+  const location = useLocation();
+
   return (
     <nav className='hidden rounded-2xl h-5/6 w-2/6 md:flex bg-nature-altLight'>
       <ul
         className='w-full flex gap-8 text-lg items-center justify-center [&>li]:w-full [&>li]:text-center
         [&>li]:hover:text-nature-dark'
       >
-        <li>
+        <li className={`${location.pathname === '/' ? 'hidden' : ''}`}>
+          <Link to={'/'} viewTransition={true}>
+            Home
+          </Link>
+        </li>
+        <li className={`${location.pathname === '/about' ? 'hidden' : ''}`}>
           <Link to={'/about'} viewTransition={true}>
             About
           </Link>
         </li>
-        <li>
-          <Link href='#projects'>Projects</Link>
+        <li className={`${location.pathname === '/projects' ? 'hidden' : ''}`}>
+          <Link href={'#projects'} viewTransition={true}>
+            Projects
+          </Link>
         </li>
-        <li>
-          <Link href='#contact'>Contact</Link>
+        <li className={`${location.pathname === '/contact' ? 'hidden' : ''}`}>
+          <Link href={'#contact'} viewTransition={true}>
+            Contact
+          </Link>
         </li>
       </ul>
     </nav>
