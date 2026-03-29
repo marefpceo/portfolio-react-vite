fdbddfd192b9c36b8b3af1457175de528f6afb4b
marefpceo <marefpceo@gmail.com>
2026-03-29 08:55:28 -0700

    Moved background image to css file

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <div
-      className='container min-w-full h-full bg-[url(./assets/portfolioBg-Desktop.webp)] bg-cover
-      bg-center 2xl:bg-[url(./assets/portfolioBg-4k.webp)]'
-    >
+    <div className='container min-w-full h-full'>
       <div role='main' className='md:max-w-full lg:max-w-4/5 mx-auto'>
         <Header />
         <Outlet />
