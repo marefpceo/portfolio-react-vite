f39c89af7676a23db15b95c63a27209954819249
marefpceo <marefpceo@gmail.com>
2026-04-06 21:38:53 -0700

    Contact form messages send test passed

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,10 +1,21 @@
+import { useForm, ValidationError } from '@formspree/react';
+import { useNavigate } from 'react-router';
+
 const inputStyle =
   'border border-nature-light/65 rounded-md px-4 py-2 focus:outline-none focus:ring-1 focus:ring-nature-accent';
 
 function ContactForm() {
+  const [state, handleSubmit] = useForm('xzdkozyj');
+  const navigate = useNavigate();
+
+  if (state.succeeded) {
+    navigate('/', { replace: false });
+  }
+
   return (
     <>
       <form
+        onSubmit={handleSubmit}
         className='mx-auto w-4/5 md:w-1/2 lg:w-1/3 xl:w-1/4 p-2 flex flex-col gap-4 rounded-md bg-nature-altLight text-nature-dark font-brunoAce 
         [&>div>input]:rounded-xs [&>div>textarea]:rounded-xs'
       >
@@ -15,8 +26,10 @@ function ContactForm() {
             name='name'
             id='name'
             className={inputStyle}
+            min={2}
             required
           />
+          <ValidationError prefix='Name' field='name' errors={state.errors} />
         </div>
         <div className='flex flex-col gap-1'>
           <label htmlFor='email'>Email</label>
@@ -27,6 +40,7 @@ function ContactForm() {
             className={inputStyle}
             required
           />
+          <ValidationError prefix='Email' field='email' errors={state.errors} />
         </div>
         <div className='flex flex-wrap justify-between items-center lg:items-start [&>select]:rounded-xs'>
           <label htmlFor='subject'>Subject</label>
@@ -45,14 +59,23 @@ function ContactForm() {
             cols='30'
             rows='10'
             className={inputStyle}
-          ></textarea>
+            minLength={2}
+            required
+          />
+          <ValidationError
+            prefix='Message'
+            field='message'
+            errors={state.errors}
+          />
         </div>
 
         <div
           className='button-div mx-auto my-5 p-2 rounded-md border border-nature-dark hover:bg-nature-dark 
           hover:text-nature-light hover:border-nature-accent hover:shadow hover:shadow-nature-accent'
         >
-          <button type='submit'>Send Message</button>
+          <button type='submit' disabled={state.submitting}>
+            Send Message
+          </button>
         </div>
       </form>
     </>
