b25012a0b5278c110ddab94af68681f1e5c682fb
marefpceo <32721274+marefpceo@users.noreply.github.com>
2026-04-08 14:42:52 -0700

    Updated ProjectCard, projectList, and images
    - Added logic to display 'Read more' for truncated descriptions
    - Scaled all project card images to 600x350
    - Updated global css

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,13 +1,15 @@
+import { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router';
+import { v4 as uuidv4 } from 'uuid';
+
+import TechTextBubble from './TextBubble';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
-import TechTextBubble from './TextBubble';
-import { Link } from 'react-router';
-import { AdvancedImage } from '@cloudinary/react';
+
 import { auto } from '@cloudinary/url-gen/actions/resize';
 import cld from '../utilities/CloudinaryConfig';
-import { v4 as uuidv4 } from 'uuid';
-import { useState } from 'react';
 
 function ProjectCard({
   cardImage,
@@ -18,15 +20,32 @@ function ProjectCard({
   sourceLink,
   techStack,
 }) {
+  const textRef = useRef(null);
+  const [isClamped, setIsClamped] = useState(false);
   const [imageSrc, setImageSrc] = useState(
     processProjectImage(cardImage).toURL(),
   );
 
+  useEffect(() => {
+    function checkOverflow() {
+      if (textRef.current) {
+        setIsClamped(
+          textRef.current.scrollHeight > textRef.current.clientHeight,
+        );
+      }
+    }
+    checkOverflow();
+    window.addEventListener('resize', checkOverflow());
+    return window.removeEventListener('resize', checkOverflow());
+  }, [description]);
+
+  // Function to set and use fallback image if issues with Cloudinary occur
   function handleError(e) {
     e.targe.onerror = null;
     setImageSrc(localImage);
   }
 
+  // Creates a Cloudinary image link based on the input
   function processProjectImage(imageId) {
     const image = cld
       .image(imageId)
@@ -36,13 +55,16 @@ function ProjectCard({
     return image;
   }
 
-  console.log(processProjectImage(cardImage).toURL());
-
   return (
-    <div className='h-fit p-0 border border-nature-accent/65 rounded-md w-96'>
+    <div className='p-0 flex flex-col border border-nature-accent/65 rounded-md w-96'>
       <div className='image-div bg-nature-dark rounded-t-md *:rounded-t-md'>
-        <div className='h-60 rounded-t-md'>
-          <img src={imageSrc} alt='' onError={handleError} />
+        <div className='rounded-t-md'>
+          <img
+            src={imageSrc}
+            alt={`${title} cover image`}
+            onError={handleError}
+            className='h-full w-full rounded-t-md'
+          />
         </div>
         <div className='px-1 pt-3 flex flex-wrap justify-start gap-4'>
           {techStack.map((tech) => (
@@ -56,17 +78,28 @@ function ProjectCard({
         </div>
       </div>
       <div
-        className='card-body-wrapper p-2 bg-linear-to-t from-nature-light via-70% via-nature-altLight 
-        to-nature-dark rounded-b-md'
+        className='card-body-wrapper p-2 flex flex-col flex-1 bg-linear-to-t from-nature-light via-70% 
+          via-nature-altLight to-nature-dark rounded-b-md'
       >
-        <div className='card-body'>
+        <div className='card-body flex-1 relative'>
           <h2 className='p-1 text-2xl text-nature-light text-shadow-xs text-shadow-nature-dark'>
             {title}
           </h2>
           <hr className='mt-1 border border-nature-dark shadow-[0_0px_5px_0] shadow-nature-accent' />
-          <p className='mt-4 line-clamp-4'>{description}</p>
+          <p ref={textRef} className='mt-4 line-clamp-4'>
+            {description}
+          </p>
+          {isClamped && (
+            <a
+              href='#more'
+              className='absolute right-0 pr-1 text-nature-dark hover:text-nature-accent 
+                hover:text-shadow-nature-dark'
+            >
+              (Read more)
+            </a>
+          )}
         </div>
-        <div className='card-footer flex gap-4 mt-2 py-2 '>
+        <div className='card-footer flex gap-4 mt-8 py-2 '>
           <Link to={sourceLink} target='_blank'>
             <FontAwesomeIcon
               icon={faGithub}
--- src/helpers/projectList.js
+++ src/helpers/projectList.js
@@ -6,7 +6,7 @@ export const projectList = [
     projectDescription:
       'The purpose of this project was to create a simple shopping cart using React to show proficiency in basic concepts.',
     projectImage: '/lastStopShop.webp',
-    projectImageUrl: 'lastStopShop_anih5y',
+    projectImageUrl: 'lastStopShop_i4azwe',
     projectLinks: [
       {
         name: 'Live Demo',
@@ -22,9 +22,9 @@ export const projectList = [
   {
     projectTitle: 'Grocery Inventory App',
     projectDescription:
-      "Express Inventory App is an inventory management web application designed for a fictitious grocery store. The app makes use of CRUD methods for managing, maintaining and updating the store's inventory. The app aims to give users a quick and efficient way view and manage inventories, organizing items by categories and subcategories",
+      "Grocery Inventory App is an inventory management web application designed for a fictitious grocery store. The app makes use of CRUD methods for managing, maintaining and updating the store's inventory. The app aims to give users a quick and efficient way view and manage inventories, organizing items by categories and subcategories",
     projectImage: '/expressInventoryApp.webp',
-    projectImageUrl: 'expressInventoryApp_dl1eii',
+    projectImageUrl: 'expressInventoryApp_m7fkfz',
     projectLinks: [
       {
         name: 'Live Demo',
@@ -42,7 +42,7 @@ export const projectList = [
     projectDescription:
       'A photo tagging app based off of the popular, classic game.',
     projectImage: '/waldoLogo.webp',
-    projectImageUrl: 'waldoLogo_jwcefc',
+    projectImageUrl: 'waldoLogo_vvfevg',
     projectLinks: [
       {
         name: 'Live Demo',
